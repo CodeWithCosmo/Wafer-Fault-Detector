3a10977978bc424ff78d1b5bfdfbc8d463b22ff4
Lalit Singh Bisht <lb5470272@gmail.com>
2023-08-25 13:11:30 +0530

    Inital commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="CWC",
+    author_email="lb5470272@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
+
